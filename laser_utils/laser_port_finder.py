572bdf095387e491520c837e51261d2921b81c0d
CarlosVivingz <jmfoliveira110@gmail.com>
2022-10-25 15:35:11 +0100

    Remove

--- laser_utils/laser_port_finder.py
+++
@@ -1,9 +0,0 @@
-import serial.tools.list_ports
-
-def port_finder():
-    ports = serial.tools.list_ports.comports()
-    output='' 
-    for port, desc, hwid in sorted(ports):
-        if 'serial' in desc.lower():
-            output=port
-    return output
