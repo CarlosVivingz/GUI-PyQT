b1f9fd1d47a3d65ac809e4b1d351ebb5173f7dc8
CarlosVivingz <jmfoliveira110@gmail.com>
2022-10-25 16:06:07 +0100

    Interface platform work

--- Interface.py
+++ Interface.py
@@ -1,11 +1,13 @@
 import sys
+import os
 
-sys.path.append(r'C:\Users\INESC\Desktop\João Oliveira\Programas\Final\lone_widgets')
-#sys.path.append(r'C:\Users\INESC\Desktop\João Oliveira\Programas\Final\lone_widgets\aux_scripts\laser_utils')
+sys.path.append(os.path.join(os.getcwd(),'lone_widgets'))
+
 #interface 2.0
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+import qdarkstyle
 from lone_widgets.Calibration_Mode_Widget import calibration_mode
 from lone_widgets.Live_Mode_Widget import live_mode
 
@@ -18,14 +20,15 @@ class interface(QWidget):
     def __init__(self):
         super().__init__()
         
-        
         self.setWindowTitle("Ferdinand")
         
         #layout principal
         self.global_layout = QHBoxLayout(self)
         
         #initiate tabs
+        splash.showMessage('Seting up Live Mode...')
         self.live=live_mode()
+        splash.showMessage('Seting up Calibration Mode...')
         self.calibration=calibration_mode()
         
         self.tabs = QTabWidget()
@@ -33,6 +36,7 @@ class interface(QWidget):
         self.tab2 = QWidget()
         self.tab3 = QWidget()
         self.tab4 = QWidget()
+        
         
         self.tabs.addTab(self.tab1,"Live Mode")
         self.tabs.addTab(self.tab2,"Calibration Mode")
@@ -53,7 +57,7 @@ class interface(QWidget):
         self.tab2.setLayout(self.tab2.layout)
         self.global_layout.addWidget(self.tabs)
         self.setLayout(self.global_layout)
-        
+    
         
     def closeEvent(self, event):
         close = QMessageBox.question(self,"Quit","Are you sure want to stop process?",QMessageBox.Yes | QMessageBox.No)
@@ -65,17 +69,26 @@ class interface(QWidget):
         
 ###################################################################
 qapp = QCoreApplication.instance()
+qapp.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+
 if qapp is None:
     qapp = QApplication(sys.argv)
     #file = QFile(":/dark/stylesheet.qss")
-    file.open(QFile.ReadOnly | QFile.Text)
-    stream = QTextStream(file)
-    cmdqapp.setStyleSheet(stream.readAll())
+    #file.open(QFile.ReadOnly | QFile.Text)
+    #stream = QTextStream(file)
     
 if __name__ == "__main__": 
+    
+    pixmap= QPixmap('transferir.jpg')
+    splash= QSplashScreen(pixmap)
+    splash.show()
+    splash.showMessage('Loading...')
+    
     #start the widget
     ui = interface()
+    splash.showMessage('Starting...')
     #show the widget
     ui.show()
+    splash.finish(ui)
     #start the event loop
     qapp.exec_()
